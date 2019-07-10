6daf4fa838443351f0566e1ae80f55fc80d36547
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-07-10 13:37:01 +0300

    Updates

---
+++ precache-manifest.fb851d86e2571de06e48591cee18cf7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd7f67ee8216c50ac8f0e27378af7bd",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "8c9261b70266ff6e0f4d",
+    "url": "/github-users/static/css/main.32f4cb51.chunk.css"
+  },
+  {
+    "revision": "24d57ada2be246db1407",
+    "url": "/github-users/static/js/2.385cf33c.chunk.js"
+  },
+  {
+    "revision": "8c9261b70266ff6e0f4d",
+    "url": "/github-users/static/js/main.8ea4b6da.chunk.js"
+  },
+  {
+    "revision": "3e4de07e46928e832bda",
+    "url": "/github-users/static/js/runtime~main.ade1b240.js"
+  }
+]);
